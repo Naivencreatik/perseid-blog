67cce5e53ad1df54bcba4fe89f5d35a8f8100b8c
Sylvain Gizard <sylvain.gizard@gmail.com>
2013-11-06 22:19:27 +0000

    Render admin items only when active

--- client/blog.js
+++ client/blog.js
@@ -1,20 +1,29 @@
 Perseid.subs.posts = Meteor.subscribe("posts");
 
+function yieldAdminItems() {
+    if (Meteor.userId()) {
+        this.render("adminPostItems", {to: "adminActions"})
+    }
+}
+
 Router.map(function(){
     this.route("postList", {
         path: "/posts",
         waitOn: Perseid.subs.posts,
+        after: yieldAdminItems
     });
 
     this.route("adminPostList", {
         path: "/admin/posts",
-        waitOn: Perseid.subs.posts
+        waitOn: Perseid.subs.posts,
+        after: yieldAdminItems
     });
 
     this.route("adminPostEditor", {
         template: "postEditor",
         path: "/admin/posts/editor/:_id?",
         waitOn: Perseid.subs.posts,
+        after: yieldAdminItems,
         data: function () {
             if (!this.params._id) {
                 return;
@@ -23,12 +32,6 @@ Router.map(function(){
             return Perseid.colls.posts.findOne({_id: this.params._id});
         }
     });
-});
-
-Router.after(function(){
-    if (Meteor.userId()) {
-        this.render("adminPostItems", {to: "adminActions"})
-    }
 });
 
 moment.lang("fr");
